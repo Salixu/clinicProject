fa1ebf8dc23a9e3a29b85bc4d7d7fcec22b68fc1
Bartosz Glanowski <bartoszglanowski@gmail.com>
2019-04-30 20:58:46 +0200

    added script which makes another table

--- DBscript.sql
+++ DBscript.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `clinicdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci ;
-USE `simpledb` ;
+USE `clinicdb` ;
 
 -- -----------------------------------------------------
 -- Table `simpledb`.`raty`
@@ -16,7 +16,14 @@ CREATE TABLE IF NOT EXISTS `clinicdb`.`users` (
   `email` VARCHAR(45) NULL,
   `phone_number` VARCHAR(45) NULL,
   `role` VARCHAR(1) NULL,
-  PRIMARY KEY (`id_user`))
+  PRIMARY KEY (`id_user`));
+
+  CREATE TABLE IF NOT EXISTS `clinicdb`.`usersRoles` (
+    `id_user_role` INT NOT NULL AUTO_INCREMENT,
+    `id_role` VARCHAR(1) NULL,
+    `id_user` VARCHAR(45) NULL,
+    PRIMARY KEY (`id_user_role`))
+
 ENGINE = InnoDB;
 
 
