1268e1e275d655b31323a31765516df426a62dd8
Bartosz Glanowski <bartoszglanowski@gmail.com>
2019-05-21 15:00:00 +0200

    add connection of name and surname string because had problems with polish characters in db, surname was mising with polish char

--- app/controllers/loginCtrl.class.php
+++ app/controllers/loginCtrl.class.php
@@ -1,16 +1,44 @@
 <?php
 namespace app\controllers;
+
 use app\forms\LoginForm;
+
 class loginCtrl{
+  private $form;
 
-public function action_loginShow(){
-  $this->generateView();
-}
+  public function __construct(){
+    $this->form = new LoginForm();
+  }
 
-public function generateView(){
-  getSmarty()->assign('page_title', 'Panel logowania');
-  getSmarty()->assign('page_description', 'Panel logowania');
-  getSmarty()->display('loginview.tpl');
-}
+  public function getParams(){
+    $this->form->email = getFromRequest('email');
+    $this->form->pass  = getFromRequest('pass');
+  }
+
+  public function action_loginShow(){
+    $this->generateView();
+  }
+
+  public function validate(){
+    if (! (isset ($this->form->email) && isset ($this->form->pass))){
+    return false;
+    }
+    $emails = getDB()->select("user",["email"],[
+      $this->form->email
+    ]);
+    //$hash = getDB()->select("user", [])
+    if ($emails != $this->form->email){
+      getMessages()->addError('Email lub haslo są nieprawidlowe');
+    }
+  }
+
+
+
+  public function generateView(){
+    getSmarty()->assign('page_title', 'Panel logowania');
+    getSmarty()->assign('page_description', 'Panel logowania');
+    getSmarty()->assign('form', $this->form);
+    getSmarty()->display('loginview.tpl');
+  }
 }
  ?>
