d0d04a5baf6c90be735d6aa8bb1bfc7a2d4fb8e0
Bartosz Glanowski <bartoszglanowski@gmail.com>
2019-05-23 20:37:39 +0200

    create page redirecting to main page after reg with mobirise

---
+++ app/views/successReg.tpl
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.8.8, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.8, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="{$conf->app_url}/assets/images/logo2.png" type="image/x-icon">
+  <meta name="description" content="Web Site Builder Description">
+  <title>Page 1</title>
+  <link rel="stylesheet" href="{$conf->app_url}/assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/theme/css/style.css">
+  <link rel="stylesheet" href="{$conf->app_url}/assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+
+</head>
+<body>
+  <section class="cid-rrjPCukuvW mbr-fullscreen mbr-parallax-background" id="header2-7">
+
+
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(3, 2, 21);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"><span style="font-weight: normal;">
+                    Dziękujemy za zarejestrowanie się w naszej klinice</span></h1>
+
+
+                <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-4" href="{$conf->action_root}mainPageshow"><span class="mobi-mbri mobi-mbri-undo mbr-iconfont mbr-iconfont-btn"></span>Wróć na stronę główną</a></div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/n">free simple web templates</a></section><script src="{$conf->app_url}/assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="{$conf->app_url}/assets/popper/popper.min.js"></script>
+  <script src="{$conf->app_url}/assets/tether/tether.min.js"></script>
+  <script src="{$conf->app_url}/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="{$conf->app_url}/assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="{$conf->app_url}/assets/parallax/jarallax.min.js"></script>
+  <script src="{$conf->app_url}/assets/theme/js/script.js"></script>
+
+
+</body>
+</html>
---
+++ templates_c/439ff0a105226df9fdaf0c2977c2b6e171f233c9_0.file.successReg.tpl.php
@@ -0,0 +1,112 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-05-23 20:37:25
+  from 'D:\xamp\htdocs\clinicProject\app\views\successReg.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ce6e865d22279_68722134',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '439ff0a105226df9fdaf0c2977c2b6e171f233c9' => 
+    array (
+      0 => 'D:\\xamp\\htdocs\\clinicProject\\app\\views\\successReg.tpl',
+      1 => 1558636638,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ce6e865d22279_68722134 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.8.8, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.8, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/images/logo2.png" type="image/x-icon">
+  <meta name="description" content="Web Site Builder Description">
+  <title>Page 1</title>
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/theme/css/style.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+
+</head>
+<body>
+  <section class="cid-rrjPCukuvW mbr-fullscreen mbr-parallax-background" id="header2-7">
+
+
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(3, 2, 21);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"><span style="font-weight: normal;">
+                    Dziękujemy za zarejestrowanie się w naszej klinice</span></h1>
+
+
+                <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-4" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+mainPageshow"><span class="mobi-mbri mobi-mbri-undo mbr-iconfont mbr-iconfont-btn"></span>Wróć na stronę główną</a></div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/n">free simple web templates</a></section><?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/web/assets/jquery/jquery.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/popper/popper.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/tether/tether.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/bootstrap/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/smoothscroll/smooth-scroll.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/parallax/jarallax.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/assets/theme/js/script.js"><?php echo '</script'; ?>
+>
+
+
+</body>
+</html>
+<?php }
+}
